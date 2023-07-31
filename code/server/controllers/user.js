f4d8271e0fb81362c9243d7947daee4929230bca
pham truc <pttruc.19it3@vku.udn.vn>
2023-08-01 02:10:59 +0700

    test v8

--- code/server/controllers/user.js
+++ code/server/controllers/user.js
@@ -38,15 +38,15 @@ const login = asyncHandler(async (req, res) => {
     const response = await User.findOne({ email })
     if (response && await response.isCorrectPassword(password)) {
         // Tách password và role ra khỏi response
-        const { password, role, ...userData } = response.toObject()
+        const { password, role, refreshToken, ...userData } = response.toObject()
         // Tạo access token
         const accessToken = generateAccessToken(response._id, role)
         // Tạo refresh token
-        const refreshToken = generateRefreshToken(response._id)
+        const newRefreshToken = generateRefreshToken(response._id)
         // Lưu refresh token vào database
-        await User.findByIdAndUpdate(response._id, { refreshToken }, { new: true })
+        await User.findByIdAndUpdate(response._id, { refreshToken: newRefreshToken }, { new: true })
         // Lưu refresh token vào cookie
-        res.cookie('refreshToken', refreshToken, { httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000 })
+        res.cookie('refreshToken', newRefreshToken, { httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000 })
         return res.status(200).json({
             sucess: true,
             accessToken,
@@ -61,7 +61,7 @@ const getCurrent = asyncHandler(async (req, res) => {
     const user = await User.findById(_id)
     //.select('-refreshToken -password -role')
     return res.status(200).json({
-        success: false,
+        success: user ? true : false,
         rs: user ? user : 'User not found'
     })
 })
@@ -119,7 +119,7 @@ const forgotPassword = asyncHandler(async (req, res) => {
 
 const resetPassword = asyncHandler(async (req, res) => {
     const { password, token } = req.body
-    if(!password || !token) throw new Error('missing input');
+    if (!password || !token) throw new Error('missing input');
     const passwordResetToken = crypto.createHash('sha256').update(token).digest('hex')
     const user = await User.findOne({ passwordResetToken, passwordResetExpires: { $gt: Date.now() } })
     if (!user) throw new Error('Invalid reset token')
@@ -133,7 +133,44 @@ const resetPassword = asyncHandler(async (req, res) => {
         mes: user ? 'update password' : " something went wrong"
     })
 
-})  
+})
+const getUsers = asyncHandler(async (req, res) => {
+    const response = await User.find().select('-refreshToken -password -role')
+    return res.status(200).json({
+        success: response ? true : false,
+        users: response
+    })
+})
+
+const deleteUser = asyncHandler(async (req, res) => {
+    const {_id}=req.query
+    if(!_id) throw new Error(' missing inputs')
+    const response = await User.findByIdAndDelete(_id)
+    return res.status(200).json({
+        success: response ? true : false,
+        deleteUser: response ?`user with mail  ${response.user} delete ` : " no user deleted"
+    })
+})
+
+const updateUser = asyncHandler(async (req, res) => {
+    const {_id}=req.user
+    if(!_id || Object.keys(req.body).length===0) throw new Error(' missing inputs')
+    const response = await User.findByIdAndUpdate(_id,req.body,{new: true}).select('password - role')
+    return res.status(200).json({
+        success: response ? true : false,
+        UpdateUser: response ?response: " something wrong happened"
+    })
+})
+
+const updateUserByAdmin = asyncHandler(async (req, res) => {
+    const {uid}=req.params
+    if(Object.keys(req.body).length===0) throw new Error(' missing inputs')
+    const response = await User.findByIdAndUpdate(uid,req.body,{new: true}).select('password - role - refreshToken')
+    return res.status(200).json({
+        success: response ? true : false,
+        UpdateUser: response ?response: " something wrong happened"
+    })
+})
 
 module.exports = {
     register,
@@ -142,5 +179,9 @@ module.exports = {
     refreshAccessToken,
     logout,
     forgotPassword,
-    resetPassword
+    resetPassword,
+    getUsers,
+    deleteUser,
+    updateUser,
+    updateUserByAdmin
 }
