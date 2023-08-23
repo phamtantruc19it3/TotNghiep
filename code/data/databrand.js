7d6be2fb359d3b7dc97e72b5652541ab34db12f7
phamtantruc19it3 <pttruc.19it3@vku.udn.vn>
2023-08-24 00:46:59 +0700

    done databse

---
+++ code/data/databrand.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        cate: 'smartphone',
+        brand: ['Apple', 'samsung', 'LG', 'Asus', 'Nokia', 'BlackBerry']
+    },
+    {
+        cate: 'Tablet',
+        brand: ['IPad', 'samsung', 'Acer', 'Asus', 'Lenovo', 'Haier']
+    },
+    {
+        cate: 'Laptop',
+        brand: ['Asus', 'Dell', 'Acer', 'HP', 'Lenovo', 'Macbook']
+    },
+    {
+        cate: 'Accessoríe',
+        brand: ['Smartswatches', 'headphone', 'Bluetooth', 'Keyboard', 'Mouse', 'Cases']
+    },
+    {
+        cate: 'Televisions',
+        brand: ['Apple', 'samsung', 'LG', 'Asus', 'Nokia', 'BlackBerry']
+    },
+    {
+        cate: 'Printer',
+        brand: ['Samsung', 'Acer', 'Asus', 'Lenovo', 'Haier']
+    },
+]
+
+module.exports=data
